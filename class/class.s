af3e3779c99aab241ee06133a96f6fc5e99c22da
zhendewokusi <768672249@qq.com>
2023-05-17 11:51:08 +0800

    learn how to compile

---
+++ class/class.s
@@ -0,0 +1,233 @@
+	.file	"class.cpp"
+	.text
+	.section	.text._ZN3Log9SettLevelEi,"axG",@progbits,_ZN3Log9SettLevelEi,comdat
+	.align 2
+	.weak	_ZN3Log9SettLevelEi
+	.type	_ZN3Log9SettLevelEi, @function
+_ZN3Log9SettLevelEi:
+.LFB1981:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movl	%esi, -12(%rbp)
+	movq	-8(%rbp), %rax
+	movl	-12(%rbp), %edx
+	movl	%edx, 12(%rax)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1981:
+	.size	_ZN3Log9SettLevelEi, .-_ZN3Log9SettLevelEi
+	.section	.rodata
+.LC0:
+	.string	"[Warning]:"
+	.section	.text._ZN3Log4WarnEPKc,"axG",@progbits,_ZN3Log4WarnEPKc,comdat
+	.align 2
+	.weak	_ZN3Log4WarnEPKc
+	.type	_ZN3Log4WarnEPKc, @function
+_ZN3Log4WarnEPKc:
+.LFB1982:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	4(%rax), %eax
+	cmpl	%eax, %edx
+	jl	.L4
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	movq	%rdx, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+.L4:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1982:
+	.size	_ZN3Log4WarnEPKc, .-_ZN3Log4WarnEPKc
+	.section	.rodata
+.LC1:
+	.string	"[Error]:"
+	.section	.text._ZN3Log5ErrorEPKc,"axG",@progbits,_ZN3Log5ErrorEPKc,comdat
+	.align 2
+	.weak	_ZN3Log5ErrorEPKc
+	.type	_ZN3Log5ErrorEPKc, @function
+_ZN3Log5ErrorEPKc:
+.LFB1983:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	(%rax), %eax
+	cmpl	%eax, %edx
+	jl	.L7
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	movq	%rdx, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+.L7:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1983:
+	.size	_ZN3Log5ErrorEPKc, .-_ZN3Log5ErrorEPKc
+	.section	.rodata
+.LC2:
+	.string	"[Info]:"
+	.section	.text._ZN3Log4InfoEPKc,"axG",@progbits,_ZN3Log4InfoEPKc,comdat
+	.align 2
+	.weak	_ZN3Log4InfoEPKc
+	.type	_ZN3Log4InfoEPKc, @function
+_ZN3Log4InfoEPKc:
+.LFB1984:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-8(%rbp), %rax
+	movl	12(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	8(%rax), %eax
+	cmpl	%eax, %edx
+	jl	.L10
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	movq	%rdx, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+.L10:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1984:
+	.size	_ZN3Log4InfoEPKc, .-_ZN3Log4InfoEPKc
+	.section	.rodata
+.LC3:
+	.string	"hello"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1985:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -32(%rbp)
+	movl	$1, -28(%rbp)
+	movl	$2, -24(%rbp)
+	movl	$2, -20(%rbp)
+	movl	-32(%rbp), %edx
+	leaq	-32(%rbp), %rax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	_ZN3Log9SettLevelEi
+	leaq	-32(%rbp), %rax
+	leaq	.LC3(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZN3Log5ErrorEPKc
+	leaq	-32(%rbp), %rax
+	leaq	.LC3(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZN3Log4WarnEPKc
+	leaq	-32(%rbp), %rax
+	leaq	.LC3(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZN3Log4InfoEPKc
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L13
+	call	__stack_chk_fail@PLT
+.L13:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1985:
+	.size	main, .-main
+	.section	.rodata
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE:
+	.byte	1
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedImEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedImEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedImEE:
+	.byte	1
+	.type	_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE, @object
+	.size	_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE, 1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE:
+	.byte	1
+	.ident	"GCC: (GNU) 13.1.1 20230429"
+	.section	.note.GNU-stack,"",@progbits
