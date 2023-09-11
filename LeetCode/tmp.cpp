768caeacefeac94e92b391dbc2a41798d71d09b7
zhendewokusi <768672249@qq.com>
2023-09-11 21:15:10 +0800

    update

--- LeetCode/tmp.cpp
+++ LeetCode/tmp.cpp
@@ -20,3 +20,37 @@ public:
     }
 };
 
+
+class Solution1 {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {searchLeftOrRightBound(nums, target, "left"), searchLeftOrRightBound(nums, target, "right")};
+    }
+private:
+    int searchLeftOrRightBound(vector<int>& nums, int target, const string& bound) {
+        int left = 0, right = nums.size() - 1;
+        int res = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                res = mid;
+                if (bound == "left") {
+                    right = mid - 1;
+                }
+                else if (bound == "right") {
+                    left = mid + 1;
+                }
+                else {
+                    // 异常处理段
+                }
+            }
+        }
+        return res;
+    }
+};
